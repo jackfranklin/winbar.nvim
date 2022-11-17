096f80126a4f499cff6ff05129dcb1ec5edfc53a
Jack Franklin <jacktfranklin@chromium.org>
2022-11-17 15:02:08 +0000

    changes for me

--- lua/winbar/config.lua
+++ lua/winbar/config.lua
@@ -1,45 +1,40 @@
 local M = {}
 
 M.defaults = {
-    enabled = false,
+	show_file_path = true,
 
-    show_file_path = true,
-    show_symbols = true,
+	colors = {
+		path = "#e6e9ef",
+		file_name = "red",
+	},
 
-    colors = {
-        path = '',
-        file_name = '',
-        symbols = '',
-    },
+	icons = {
+		seperator = ">",
+		editor_state = "●",
+		lock_icon = "",
+	},
 
-    icons = {
-        file_icon_default = '',
-        seperator = '>',
-        editor_state = '●',
-        lock_icon = '',
-    },
-
-    exclude_filetype = {
-        'help',
-        'startify',
-        'dashboard',
-        'packer',
-        'neogitstatus',
-        'NvimTree',
-        'Trouble',
-        'alpha',
-        'lir',
-        'Outline',
-        'spectre_panel',
-        'toggleterm',
-        'qf',
-    }
+	exclude_filetype = {
+		"help",
+		"startify",
+		"dashboard",
+		"packer",
+		"neogitstatus",
+		"NvimTree",
+		"Trouble",
+		"alpha",
+		"lir",
+		"Outline",
+		"spectre_panel",
+		"toggleterm",
+		"qf",
+	},
 }
 
 M.options = {}
 
 function M.set_options(opts)
-	M.options = vim.tbl_deep_extend('force', {}, M.defaults, opts or {})
+	M.options = vim.tbl_deep_extend("force", {}, M.defaults, opts or {})
 end
 
 return M
--- lua/winbar/init.lua
+++ lua/winbar/init.lua
@@ -1,19 +1,20 @@
-local config = require('winbar.config')
+local config = require("winbar.config")
 
 local M = {}
 
 function M.setup(opts)
-    config.set_options(opts)
-    local winbar = require('winbar.winbar')
+	config.set_options(opts)
+	local winbar = require("winbar.winbar")
 
-    winbar.init()
-    if opts.enabled == true then
-        vim.api.nvim_create_autocmd({ 'DirChanged', 'CursorMoved', 'BufWinEnter', 'BufFilePost', 'InsertEnter', 'BufWritePost' }, {
-            callback = function()
-                winbar.show_winbar()
-            end
-        })
-    end
+	winbar.init()
+	vim.api.nvim_create_autocmd(
+		{ "DirChanged", "CursorMoved", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost" },
+		{
+			callback = function()
+				winbar.show_winbar()
+			end,
+		}
+	)
 end
 
 return M
